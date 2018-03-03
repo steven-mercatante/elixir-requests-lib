0efb6cd61dce94201a13142e9764b1b768e70b60
Steven Mercatante <steven.mercatante@gmail.com>
2018-03-02 21:09:32 -0500

    Call maybe_decode_response_body from post_json

--- lib/requests.ex
+++ lib/requests.ex
@@ -29,7 +29,10 @@ defmodule Requests do
     def post_json(url, body, headers \\ [], options \\ []) do
         body = Poison.encode!(body)
         headers = Keyword.put(headers, :"Content-Type", "application/json")
-        post(url, body, headers, options)
+        case post(url, body, headers, options) do
+            {:ok, response} -> {:ok, maybe_decode_response_body(response)}
+            {:error, %Error{reason: reason}} -> raise Error, reason: reason
+        end
     end
 
     defp maybe_decode_response_body(response) do
