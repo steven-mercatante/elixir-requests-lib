d35e213ea77f6ced1e17cd60248fc469b3a016e3
Steven Mercatante <steven.mercatante@gmail.com>
2018-03-02 21:07:17 -0500

    Downcase header keys when checking for Content-Type

--- lib/requests.ex
+++ lib/requests.ex
@@ -4,18 +4,6 @@ defmodule Requests do
     alias HTTPoison.Error
     alias HTTPoison.Response
 
-    def maybe_decode_response_body(response) do
-        content_type =
-            response.headers
-            |> Enum.into(%{})
-            |> Map.get("Content-Type", "")  # does this key need to be case-insensitive?
-
-        case String.downcase(content_type) =~ "application/json" do
-            true -> %Response{response | body: Poison.decode!(response.body)}
-            false -> response
-        end
-    end
-
     def get_json!(url, headers \\ [], options \\ []) do
         case get_json(url, headers, options) do
             {:ok, response} -> response
@@ -42,5 +30,18 @@ defmodule Requests do
         body = Poison.encode!(body)
         headers = Keyword.put(headers, :"Content-Type", "application/json")
         post(url, body, headers, options)
+    end
+
+    defp maybe_decode_response_body(response) do
+        content_type =
+            response.headers
+            |> Enum.map(fn {k, v} -> {String.downcase(k), v} end)
+            |> Enum.into(%{})
+            |> Map.get("content-type", "")
+
+        case String.downcase(content_type) =~ "application/json" do
+            true -> %Response{response | body: Poison.decode!(response.body)}
+            false -> response
+        end
     end
 end
